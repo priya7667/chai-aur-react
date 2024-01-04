0812d6bd8f3314ed54e4c248369a5d1ecc8d8ac3
priya7667 <priyanjalibarriar091@gmail.com>
2024-01-04 12:42:50 +0530

    basics of react like rendering the component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai";
+
+function App() {
+  return(
+    <>
+    <Chai/>
+    <h1>chai aur react with vite|Priyanjali</h1>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Chai.jsx
@@ -0,0 +1,8 @@
+function Chai()
+{
+    return(
+        <h2>Hello new function</h2>
+    )
+
+}
+export default Chai;
